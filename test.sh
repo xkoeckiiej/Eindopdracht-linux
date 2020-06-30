2dbeeec5248b1d101d5a41211e06fed7181d0e75
Youri <Youri.sanders@hotmail.com>
2020-06-30 15:05:45 +0200

    muninip

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
-MuninNaam=$(salt 'itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '.+?(?=:)')
-MuninIP=$(salt 'itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '10.0.[0-9]{1,3}.[0-9]{1,3}')
+MuninNaam=$(salt -C '* not itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '.+?(?=:)')
+MuninIP=$(salt -C '* not itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '10.0.[0-9]{1,3}.[0-9]{1,3}')
 
 echo $MuninNaam
 echo $MuninIP
+
+for word in $MuninNaam
+do
+echo $word
+done
+
+for ip in $MuninIP
+do
+echo $ip
+done
