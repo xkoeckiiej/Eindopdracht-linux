3d2cea8dc160fdbb4a47799b46b15a2ce6e1cfd9
Youri <Youri.sanders@hotmail.com>
2020-06-30 14:50:31 +0200

    naam en ip munin minion

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
-Variabele=$(salt 'itv2g-ubu-1.internal.cloudapp.net' network.ip_addrs type=private)
-#echo $Variabele
-echo $Variabele | grep -oP '10.0.[0-9]{1,3}.[0-9]{1,3}'
+MuninNaam=$(salt 'itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '.+?(?=:)')
+MuninIP=$(salt 'itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '10.0.[0-9]{1,3}.[0-9]{1,3}')
+
+echo $MuninNaam
+echo $MuninIP
