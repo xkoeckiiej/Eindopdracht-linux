22ce1ecfb919587e438aac6d08451e94b5181b92
Youri <Youri.sanders@hotmail.com>
2020-06-30 16:58:40 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -20,21 +20,18 @@ MuninIP=($MuninIP)
 #done
 
 
-for ((i=0; i<${#MuninNaam[@]};++i)); do
+for ((i=0; i<${#MuninNaam[@]};++i));
+do
 echo "${MuninNaam[i]}";
 echo "${MuninIP[i]}"
-echo "[${MuninNaam[i]}]" >> test.sh
-echo "address ${MuninIP[i]}" >> test.sh
-echo "use_node_name_yes" >> test.sh
+echo "[${MuninNaam[i]}]" >> testlog.sh
+echo "address ${MuninIP[i]}" >> testlog.sh
+echo "use_node_name_yes" >> testlog.sh
 done
-
 exit 0
-#[itv2g-ubu-30.internal.cloudapp.net]
-#address 10.0.7.49
-#use_node_name_yes
-#[itv2g-ubu-1.internal.cloudapp.net]
-#address 10.0.7.78
-#use_node_name_yes
-#[itv2g-ubu-2.internal.cloudapp.net]
-#address 10.0.7.50
-#use_node_name_yes
+
+#muninbarrel = "${MuninNaam[i]}"
+#if grep -Fxqe "$muninbarrel" /srv/salt/testlog.sh;
+#then echo "gevonden"
+#else echo "niet gevonden"
+#fi
