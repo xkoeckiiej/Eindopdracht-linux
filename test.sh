2358106bde33fe9ac152a8e38fcdd2d20999b891
Youri <Youri.sanders@hotmail.com>
2020-06-30 15:43:47 +0200

    Munin ip ding update

--- test.sh
+++ test.sh
@@ -1,15 +1,29 @@
 MuninNaam=$(salt -C '* not itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '.+?(?=:)')
 MuninIP=$(salt -C '* not itv2g-ubu-29.internal.cloudapp.net' network.ip_addrs type=private | grep -oP '10.0.[0-9]{1,3}.[0-9]{1,3}')
 
-echo $MuninNaam
-echo $MuninIP
+MuninNaam=($MuninNaam)
+MuninIP=($MuninIP)
 
-for word in $MuninNaam
-do
-echo $word
+#echo $MuninNaam
+#echo $MuninIP
+
+#for word in $MuninNaam
+#do
+#       echo $word
+#done
+
+
+
+#for ip in $MuninIP
+#do
+#       echo $ip
+#done
+
+
+for ((i=0; i<${#MuninNaam[@]};++i)); do
+echo "${MuninNaam[i]}";
+echo "${MuninIP[i]}"
 done
 
-for ip in $MuninIP
-do
-echo $ip
-done
+
+#echo ${MuninNaam[0]}
